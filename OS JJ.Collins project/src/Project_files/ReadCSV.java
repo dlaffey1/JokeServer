36d9c3d73b6c5ef8fad5a5e181cb3c63ad9cde86
dlaffey1 <116005942+dlaffey1@users.noreply.github.com>
2023-05-18 23:31:19 +0100

    updated version

--- OS JJ.Collins project/src/Project_files/ReadCSV.java
+++ OS JJ.Collins project/src/Project_files/ReadCSV.java
@@ -1,5 +1,5 @@
 package Project_files;
-//Conor Glynn 16th May 2023
+
 
 import java.io.BufferedReader;
 import java.io.FileReader;
@@ -14,10 +14,8 @@ public class ReadCSV {
         String path = "jokes.csv";
         String RowArray[] = new String[2];
         Random random = new Random();
-        int randomIndex = random.nextInt(3);
+        int randomIndex = random.nextInt(5);
 
-        //String Q = RowArray[0];
-        //String A = RowArray[1];
         try(BufferedReader buf = new BufferedReader(new FileReader(path))){
             //either use output of first as input of second
             //or have a function call in this class itself
@@ -27,7 +25,7 @@ public class ReadCSV {
             }
             //Read the desired row
             jokerow = buf.readLine();
-            //RowArray = br.readLine();
+            
             if(jokerow != null) {
                 //System.out.println(jokerow);
                 return jokerow;
@@ -42,11 +40,8 @@ public class ReadCSV {
         return null;
     }
 
-    public static void main(String[] args) {
+    /*public static void main(String[] args) {
         System.out.println(ReadJokeFromCSV());
-    }
+    }*/
 
-    // String ReturnQ(Array RowArray[]){
-    //     return Q;
-    // }
 }
